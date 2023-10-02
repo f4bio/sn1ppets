68fb685a7e8a2bca47bded650b09dad497393895
Fabio Tea <f4bio@users.noreply.github.com>
2023-10-02 03:53:34 +0200

    Update fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -2,8 +2,16 @@
 
 echo "Starting..."
 
-find $(pwd) -type f -exec chmod 644 "{}" \;
-find $(pwd) -type d -exec chmod 755 "{}" \;
+if ! command -v fd &> /dev/null
+then
+    echo "`fd` could not be found; https://github.com/sharkdp/fd"
+    exit 1
+fi
+
+# find $(pwd) -type f -exec chmod 644 "{}" \;
+fd --type f --exec chmod 644 {}
+# find $(pwd) -type d -exec chmod 755 "{}" \;
+fd --type d --exec chmod 755 {}
 chown -R $SUDO_UID:$SUDO_GID $(pwd)
 
 echo "Done!"
