8741ebe492db34ebc693b102507082701e115e53
Fabio Tea <iam@f4b.io>
2023-10-02 02:01:50 +0000

    save

--- debian_base-packages_install.sh
+++ debian_base-packages_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+## script-commons:
+#currentDir=$( dirname "$( command -v "$0" )" )
+#scriptsCommonUtilities="$currentDir/scripts-common/utilities.sh"
+#[ ! -f "$scriptsCommonUtilities" ] && echo -e "ERROR: scripts-common utilities not found, you must initialize your git submodule once after you cloned the repository:\ngit submodule init\ngit submodule update" >&2 && exit 1
+# shellcheck disable=1090
+#. "$scriptsCommonUtilities"
+## :script-commons
+
 echo "Starting..."
 
 apt-get --yes install zsh imagemagick ffmpeg zip unzip rar unrar ripgrep asciinema tmux git curl neovim detox
