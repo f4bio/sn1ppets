f7e1e525c58ef9a2590469df7a768e58c02512f0
Fabio Tea <f4bio@users.noreply.github.com>
2024-06-11 04:08:45 +0200

    Update fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -14,9 +14,11 @@ info "Starting..."
 
 export NEEDRESTART_MODE=a
 
-find $(pwd) -type f -exec chmod 644 "{}" \; >> /tmp/fix_permissions.log
+#find $(pwd) -type f -exec chmod o+rw,g+r,o+r "{}" \; >> /tmp/fix_permissions.log
+fdfind --type file --exec chmod o+rw,g+r,o+r >> /tmp/fix_permissions.log
 info "Files-644 Done... (1/4)"
-find $(pwd) -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log
+#find $(pwd) -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log
+fdfind --type directory --exec chmod o+rwx,g+rx,o+rx >> /tmp/fix_permissions.log
 info "Directories-755 Done... (2/4)"
 chown -R $SUDO_UID:$SUDO_GID $(pwd) >> /tmp/fix_permissions.log
 info "Chown-R Done... (3/4)"
