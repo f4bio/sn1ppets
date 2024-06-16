880184992effc0ca69f568675e2e2cf7865ca03d
Fabio Tea <f4bio@users.noreply.github.com>
2024-06-16 17:23:48 +0200

    Update debian_package_upgrade.sh

--- debian_package_upgrade.sh
+++ debian_package_upgrade.sh
@@ -8,19 +8,23 @@ BSC_VERBOSE=1
 ## :script-commons
 
 checkBin apt-get || errorMessage "This snippet requires apt-get. Install it please, and then run this tool again."
-echo "" > /tmp/debian_package_upgrade.log
+
+runDateTime=$(getFormattedDatetime '%Y-%m-%d-%H-%M-%S' )
+echo "---" > /tmp/debian_package_upgrade.log
+echo "$runDateTime" >> /tmp/debian_package_upgrade.log
+echo "---" >> /tmp/debian_package_upgrade.log
 
 info "Starting..."
 
 export NEEDRESTART_MODE=a
 
-apt-get --yes --quiet update >> /tmp/debian_package_upgrade.log
+apt-get --yes --quiet update 2> /tmp/debian_package_upgrade.log
 info "Update Done... (1/4)"
-apt-get --yes --quiet full-upgrade >> /tmp/debian_package_upgrade.log
+apt-get --yes --quiet full-upgrade 2> /tmp/debian_package_upgrade.log
 info "Full Upgrade Done... (2/4)"
-apt-get --yes --quiet autoclean >> /tmp/debian_package_upgrade.log
+apt-get --yes --quiet autoclean 2> /tmp/debian_package_upgrade.log
 info "Autoclean Done... (3/4)"
-apt-get --yes --quiet autoremove >> /tmp/debian_package_upgrade.log
+apt-get --yes --quiet autoremove 2> /tmp/debian_package_upgrade.log
 info "Autoremove Done... (4/4)"
 
 info "All Done!"
