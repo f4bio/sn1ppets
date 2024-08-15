2e7864409dc3526a850b3551657e9fb21b8791e0
Fabio Tea <f4bio@users.noreply.github.com>
2024-08-15 17:22:34 +0200

    Update debian_base-packages_install.sh

--- debian_base-packages_install.sh
+++ debian_base-packages_install.sh
@@ -13,6 +13,7 @@ echo "" > /tmp/debian_base-packages_install.log
 info "Starting..."
 
 export NEEDRESTART_MODE=a
+export DEBIAN_FRONTEND=noninteractive
 
 apt-get --yes --quiet install zsh autojump imagemagick ffmpeg zip unzip unrar-free ripgrep asciinema tmux git curl neovim detox >> /tmp/debian_base-packages_install.log
 info "Install Done... (1/1)"
