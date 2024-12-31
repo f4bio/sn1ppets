a7387aca21c233e02dbf7c3e2ebb7f4488bb1bd4
Fabio Tea <f4bio@users.noreply.github.com>
2024-12-31 01:48:34 +0100

    Update fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -15,15 +15,23 @@ info "Starting..."
 export NEEDRESTART_MODE=a
 UID="${SUDO_UID:-1000}"
 GID="${SUDO_GID:-1000}"
+DIR="${DIR:-$(pwd)}"
 
 # fdfind --type file --exec chmod o+r,g+r,u+rw >> /tmp/fix_permissions.log
-find $(pwd) -type f -exec chmod o+rw,g+r,o+r "{}" \; >> /tmp/fix_permissions.log
+debug "find $DIR -type f -exec chmod o+rw,g+r,o+r "{}" \; >> /tmp/fix_permissions.log"
+find $DIR -type f -exec chmod o+rw,g+r,o+r "{}" \; >> /tmp/fix_permissions.log
 info "Files-644 Done... (1/4)"
+
 # fdfind --type directory --exec chmod o+rx,g+rx,u+rwx >> /tmp/fix_permissions.log
-find $(pwd) -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log
+debug "find $DIR -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log"
+find $DIR -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log
 info "Directories-755 Done... (2/4)"
+
+debug "chown -R $UID:$GID $(pwd) >> /tmp/fix_permissions.log"
 chown -R $UID:$GID $(pwd) >> /tmp/fix_permissions.log
 info "Chown-R Done... (3/4)"
+
+debug "sync >> /tmp/fix_permissions.log"
 sync >> /tmp/fix_permissions.log
 info "Sync Done... (4/4)"
 
