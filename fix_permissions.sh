87a528820d5578ff0ddf21314217c6e6f2758f46
Fabio Tea <f4bio@users.noreply.github.com>
2024-12-31 01:55:39 +0100

    Update fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -13,22 +13,22 @@ echo "" > /tmp/fix_permissions.log
 info "Starting..."
 
 export NEEDRESTART_MODE=a
-UID="${SUDO_UID:-1000}"
-GID="${SUDO_GID:-1000}"
-DIR="${DIR:-$(pwd)}"
+OWNER_USER_ID="${SUDO_UID:-1000}"
+OWNER_GROUP_ID="${SUDO_GID:-1000}"
+TARGET_DIR="${DIR:-$(pwd)}"
 
 # fdfind --type file --exec chmod o+r,g+r,u+rw >> /tmp/fix_permissions.log
-writeMessage "find $DIR -type f -exec chmod o+rw,g+r,o+r "{}" \; >> /tmp/fix_permissions.log"
-find $DIR -type f -exec chmod o+rw,g+r,o+r "{}" \; >> /tmp/fix_permissions.log
+writeMessage "find $TARGET_DIR -type f -exec chmod o+rw,g+r,o+r "{}" \; >> /tmp/fix_permissions.log"
+find $TARGET_DIR -type f -exec chmod o+rw,g+r,o+r "{}" \; >> /tmp/fix_permissions.log
 info "Files-644 Done... (1/4)"
 
 # fdfind --type directory --exec chmod o+rx,g+rx,u+rwx >> /tmp/fix_permissions.log
-writeMessage "find $DIR -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log"
-find $DIR -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log
+writeMessage "find $TARGET_DIR -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log"
+find $TARGET_DIR -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log
 info "Directories-755 Done... (2/4)"
 
-writeMessage "chown -R $UID:$GID $(pwd) >> /tmp/fix_permissions.log"
-chown -R $UID:$GID $(pwd) >> /tmp/fix_permissions.log
+writeMessage "chown -R $OWNER_USER_ID:$OWNER_GROUP_ID $TARGET_DIR >> /tmp/fix_permissions.log"
+chown -R $OWNER_USER_ID:$OWNER_GROUP_ID $TARGET_DIR >> /tmp/fix_permissions.log
 info "Chown-R Done... (3/4)"
 
 writeMessage "sync >> /tmp/fix_permissions.log"
