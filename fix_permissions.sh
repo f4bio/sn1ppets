848e022494f36243ef23e57b96a0dfa89ee787c1
Fabio Tea <f4bio@users.noreply.github.com>
2024-12-31 01:46:23 +0100

    Update fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -16,11 +16,11 @@ export NEEDRESTART_MODE=a
 UID="${SUDO_UID:-1000}"
 GID="${SUDO_GID:-1000}"
 
-#find $(pwd) -type f -exec chmod o+rw,g+r,o+r "{}" \; >> /tmp/fix_permissions.log
-fdfind --type file --exec chmod o+r,g+r,u+rw >> /tmp/fix_permissions.log
+# fdfind --type file --exec chmod o+r,g+r,u+rw >> /tmp/fix_permissions.log
+find $(pwd) -type f -exec chmod o+rw,g+r,o+r "{}" \; >> /tmp/fix_permissions.log
 info "Files-644 Done... (1/4)"
-#find $(pwd) -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log
-fdfind --type directory --exec chmod o+rx,g+rx,u+rwx >> /tmp/fix_permissions.log
+# fdfind --type directory --exec chmod o+rx,g+rx,u+rwx >> /tmp/fix_permissions.log
+find $(pwd) -type d -exec chmod 755 "{}" \; >> /tmp/fix_permissions.log
 info "Directories-755 Done... (2/4)"
 chown -R $UID:$GID $(pwd) >> /tmp/fix_permissions.log
 info "Chown-R Done... (3/4)"
